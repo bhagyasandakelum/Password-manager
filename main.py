eba4e3a854b4bf04c3dafd11544b4593b549f751
bhagyasandakelum <2022csc030@univ.jfn.ac.lk>
2025-06-29 01:57:34 +0530

    first commit

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import hashlib
 import getpass
 
 password_manager = {}
+
+def create_manager():
+    username = input("Enter a username: ")
+    password = getpass.getpass("Enter a password: ")
+    hashed_password = hashlib.sha256(password.encode()).haxdigest()
+    password_manager[username] = hashed_password
+    print("account crreated successfully.")
