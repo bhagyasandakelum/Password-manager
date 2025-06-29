eafb68945331e8e3e048e97c35003d3f66aa8a84
bhagyasandakelum <2022csc030@univ.jfn.ac.lk>
2025-06-30 01:57:17 +0530

    First Release 1.0

--- main.py
+++ main.py
@@ -9,3 +9,25 @@ def create_manager():
     hashed_password = hashlib.sha256(password.encode()).haxdigest()
     password_manager[username] = hashed_password
     print("account crreated successfully.")
+
+def login():
+    username = input("Enter your username: ")
+    password = getpass.getpass("Enter your password: ")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    if username in password_manager.keys() and password_manager[username] == hashed_password:
+        print("Login successfull!")
+    else:
+        print("Invalid username or password")
+def main():
+    while True:
+        choice = input ("Enter a to create an account,  2 to login, or 0 to exist: ")
+        if choice == "1":
+            create_account()
+        elif choice == "2":
+            login()
+        elif choice == "0":
+            break
+        else:
+            print("Invalid choice.")
+if __name__ == "__main__":
+    main()
