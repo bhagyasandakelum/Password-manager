5831974bbd421ee65da7b704090923b60514ecdd
bhagyasandakelum <2022csc030@univ.jfn.ac.lk>
2025-11-15 02:16:45 +0530

    final version

--- main.py
+++ main.py
@@ -6,28 +6,32 @@ password_manager = {}
 def create_manager():
     username = input("Enter a username: ")
     password = getpass.getpass("Enter a password: ")
-    hashed_password = hashlib.sha256(password.encode()).haxdigest()
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
     password_manager[username] = hashed_password
-    print("account crreated successfully.")
+    print("Account created successfully.")
 
 def login():
     username = input("Enter your username: ")
     password = getpass.getpass("Enter your password: ")
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
-    if username in password_manager.keys() and password_manager[username] == hashed_password:
-        print("Login successfull!")
+    
+    if username in password_manager and password_manager[username] == hashed_password:
+        print("Login successful!")
     else:
         print("Invalid username or password")
+
 def main():
     while True:
-        choice = input ("Enter a to create an account,  2 to login, or 0 to exist: ")
+        choice = input("Enter 1 to create an account, 2 to login, or 0 to exit: ")
+        
         if choice == "1":
-            create_account()
+            create_manager()
         elif choice == "2":
             login()
         elif choice == "0":
             break
         else:
             print("Invalid choice.")
+
 if __name__ == "__main__":
     main()
